370a8f67e6f3a5e23e8c4b264c3cea36f9e22e27
Nathannael <nathannael2009@gmail.com>
2019-02-24 21:07:52 -0300

    Refactoring the code so it's easier on the eyes and fixing query problems
    - Fixed problem that didn't retrieve cards if the query ended in 
    parenthesis on SCG (for example, "ravnica weekend" returned no results 
    because it was in parenthesis within SCG. Fixed URL to be called on SCG.
    - Fixed problem that broke the bot when niv-mizzet was queried (it was a 
    problem with strikethrough within SCG website)

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const TelegramBot = require( `node-telegram-bot-api` );
-const rp = require('request-promise');
+const request = require('request-promise');
 const $ = require('cheerio');
 require('dotenv').config();
 
@@ -14,33 +14,57 @@ Array.prototype.groupBy = function(prop) {
   }, {})
 }
 
+var fetch_table_data = (td_class) => {
+  return (x) => { return x.attribs !== undefined && x.attribs.class.includes(td_class) }
+}
+
+var convert_price = (price, rate=2.5) => {
+  return (parseFloat(price)*rate).toFixed(2)
+}
+
+//SCG has this thing where the price is sometimes strikethrough.
+//That functions checks if td content is a number.
+//If it is, parse it. If it isn't, get the number, then parse it.
+var get_price_from_table_data = (td_content) => {
+  if(!td_content.children[0].hasOwnProperty("data")) {
+    td_content = td_content.children[2];
+  }
+  return parseFloat(td_content.children[0].data.substring(1)).toFixed(2);
+}
+
 var find_and_fetch_price = (msg, match) => {
   const chatId = msg.chat.id;
   const cardName = match[1];
-  const url = 'http://www.starcitygames.com/results?name='+encodeURI(cardName)+'&auto=Y';
-  console.log(url);
-  rp(url)
+  const url = 'http://www.starcitygames.com/results?name='+encodeURI(cardName)+'&go.x=0&go.y=0';
+
+  console.log(`${msg.chat.username} searched for: ${cardName}. URL: ${url}`);
+  request(url)
     .then(function(html){
-      cards = [];
-      for(let i = 0; i < $('.deckdbbody_row,.deckdbbody2_row', html).length; i++) {
-        if ($('.deckdbbody_row,.deckdbbody2_row', html)[i].children.filter(
-          (x) => { return x.attribs !== undefined && x.attribs.class.includes('search_results_7') }
+      let cards = [];
+      let table_rows = $('.deckdbbody_row,.deckdbbody2_row', html);
+
+      for(let i = 0; i < table_rows.length; i++) {
+        if (table_rows[i].children.filter(
+          fetch_table_data('search_results_7') // Card condition is in this column
         )[0].children[0].children[0].data == "NM/M") {
-          let td_name = $('.deckdbbody_row,.deckdbbody2_row', html)[i].children.filter(
-            (x) => { return x.attribs !== undefined && x.attribs.class.includes('search_results_1') }
-          );
-          let td_set = $('.deckdbbody_row,.deckdbbody2_row', html)[i].children.filter(
-            (x) => { return x.attribs !== undefined && x.attribs.class.includes('search_results_2') }
-          );
-          let td_value = $('.deckdbbody_row,.deckdbbody2_row', html)[i].children.filter(
-            (x) => { return x.attribs !== undefined && x.attribs.class.includes('search_results_9') }
-          )
+
+          let td_name = table_rows[i].children.filter(fetch_table_data('search_results_1'))[0]; // Card name is in this column
+          let td_set = table_rows[i].children.filter(fetch_table_data('search_results_2'))[0]; // Card set is in this column
+          let td_price = table_rows[i].children.filter(fetch_table_data('search_results_9'))[0]; // Card price is in this column
+
+          // Each td has a tree of HTML until we get to the info that we think is important.
+          // That's why we go through a bunch of children
+          let scg_link = td_name.children[0].children[0].attribs["href"];
+          let name = td_name.children[0].children[0].children[0].data;
+          let set = td_set.children[1].children[0].data;
+          let price = get_price_from_table_data(td_price);
+
           cards.push({
-            scg_link: td_name[0].children[0].children[0].attribs["href"],
-            name: td_name[0].children[0].children[0].children[0].data,
-            set: td_set[0].children[1].children[0].data,
-            value: "$" + (parseFloat(td_value[0].children[0].data.substring(1))).toFixed(2),
-            converted_value: "R$ " + (parseFloat(td_value[0].children[0].data.substring(1))*2.5).toFixed(2)
+            scg_link: scg_link,
+            name: name,
+            set: set,
+            price: `$ ${price}`,
+            converted_price: `R$ ${convert_price(price)}`
           })
 
         }
@@ -52,9 +76,10 @@ var find_and_fetch_price = (msg, match) => {
 
         for (const card_name of Object.keys(grouped_cards)) {
           message += `*${card_name.trim()}*\n`
-          message += grouped_cards[card_name].reduce((acc, current) => {
-            return `${acc}${current["set"]}: ${current["value"]} → * ${current["converted_value"]}*\n`;
-          }, "")
+          message += grouped_cards[card_name].reduce(
+            (full_string, current_card) => {
+              return `${full_string}${current_card["set"]}: ${current_card["price"]} → * ${current_card["converted_price"]}*\n`;
+            }, "")
           message += "\n"
         }
 
@@ -66,7 +91,14 @@ var find_and_fetch_price = (msg, match) => {
     })
     .catch(function(err){
       console.log(err);
+      bot.sendMessage(chatId, `Ocorreu um erro! Tente novamente mais tarde.`)
     });
 }
 
-bot.onText(/\/card (.+)/, function(msg, match) { find_and_fetch_price(msg, match) });
+bot.onText(/\/card (.+)/, function(msg, match) {
+  find_and_fetch_price(msg, match)
+
+});
+bot.onText(/\/start/, function(msg, match) {
+  bot.sendMessage(msg.chat.id, `Bem vindo! Utilize o comando */card <nome da carta>* para começar a verificar preços de cartas! A conversão segue a conversão x2,5.`, {parse_mode : "markdown"});
+});
